ff382bba888f7d9868f8b583588662bba754c0b6
Jordan Irwin <antumdeluge@gmail.com>
2017-05-18 15:06:02 -0700

    Add original code by PilzAdam:
    @ forum post updated: June 08, 2013

---
+++ README.md
@@ -0,0 +1,18 @@
+## Clean mod for [Minetest][]
+
+
+---
+### **Description:**
+
+Fork of [PilzAdam's ***clean*** mod][f.pilzadam] for Minetest.
+
+
+---
+### **Licensing:**
+
+[WTFPL](LICENSE.txt)
+
+
+[Minetest]: http://www.minetest.net/
+
+[f.pilzadam]: https://forum.minetest.net/viewtopic.php?t=2777
---
+++ init.lua
@@ -0,0 +1,28 @@
+-- clean by PilzAdam
+-- LICENSE: WTFPL
+
+local old_nodes = {"mod:a", "mod:b"}
+local old_entities = {}
+
+for _,node_name in ipairs(old_nodes) do
+    minetest.register_node(":"..node_name, {
+        groups = {old=1},
+    })
+end
+
+minetest.register_abm({
+    nodenames = {"group:old"},
+    interval = 1,
+    chance = 1,
+    action = function(pos, node)
+        minetest.env:remove_node(pos)
+    end,
+})
+
+for _,entity_name in ipairs(old_entities) do
+    minetest.register_entity(":"..entity_name, {
+        on_activate = function(self, staticdata)
+            self.object:remove()
+        end,
+    })
+end
