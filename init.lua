2404594aa34aa2c2069368718bacfa5a9e7b4765
AntumDeluge <antumdeluge@gmail.com>
2017-05-18 16:20:49 -0700

    Add function to replace old nodes

--- init.lua
+++ init.lua
@@ -4,6 +4,16 @@
 local old_nodes = {}
 local old_entities = {}
 
+-- Old/Missing nodes that should be replaced with something currently in game
+local replace_nodes = {}
+
+
+-- "Replaces" an old/non-existent node
+local function replace_node(old_node, new_node)
+    minetest.register_alias(old_node, new_node)
+end
+
+
 for _,node_name in ipairs(old_nodes) do
     minetest.register_node(':'..node_name, {
         groups = {old=1},
@@ -25,4 +35,9 @@ for _,entity_name in ipairs(old_entities) do
             self.object:remove()
         end,
     })
+end
+
+-- Replace old nodes
+for _, node_group in pairs(replace_nodes) do
+    replace_node(node_group[1], node_group[2])
 end
