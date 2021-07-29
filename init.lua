4f9e5a643418f07d0989e917a4d680a3d587a3f2
Jordan Irwin <antumdeluge@gmail.com>
2021-07-29 13:38:10 -0700

    Move pencil into tools.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
 
---- Cleaner
---
---  @topic tools
-
-
 cleaner = {}
 cleaner.modname = core.get_current_modname()
 cleaner.modpath = core.get_modpath(cleaner.modname)
@@ -41,6 +36,7 @@ local scripts = {
 	"settings",
 	"api",
 	"chat",
+	"tools",
 	"entities",
 	"nodes",
 	"items",
@@ -50,43 +46,3 @@ local scripts = {
 for _, script in ipairs(scripts) do
 	dofile(cleaner.modpath .. "/" .. script .. ".lua")
 end
-
-
-local S = core.get_translator(cleaner.modname)
-
-
-local sound_handle
-
---- Master Pencil
---
---  @tool cleaner:pencil
---  @img cleaner_pencil.png
---  @privs server
---  @usage
---  place (right-click):
---  - when not pointing at a node, changes modes
---  - when pointing at a node, sets node to be used
---
---  use (left-click):
---  - executes action for current mode:
---    - erase: erases pointed node
---    - write: adds node
---    - swap:  replaces pointed node
-core.register_tool(cleaner.modname .. ":pencil", {
-	description = S("Master Pencil"),
-	inventory_image = "cleaner_pencil.png",
-	liquids_pointable = true,
-	on_use = aux.tool.on_use,
-	on_secondary_use = aux.tool.on_secondary_use,
-	on_place = aux.tool.on_place,
-})
-
-core.register_tool(cleaner.modname .. ":pencil_1", {
-	description = S("Master Pencil"),
-	inventory_image = "cleaner_pencil.png^[transformFXFY",
-	liquids_pointable = true,
-	groups = {not_in_creative_inventory=1},
-	on_use = aux.tool.on_use,
-	on_secondary_use = aux.tool.on_secondary_use,
-	on_place = aux.tool.on_place,
-})
